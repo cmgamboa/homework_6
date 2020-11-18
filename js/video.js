f36ec172b63ccc07b2c74866fada21d9cbe49301
Chris Gamboa <cmgamboa@umich.edu>
2020-11-18 00:38:14 -0500

    Update video.js

--- js/video.js
+++ js/video.js
@@ -24,28 +24,42 @@ document.querySelector("#slower").addEventListener("click", function(){
 });
 //slower
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Pause")
+	console.log(video.playbackRate);
+	video.playbackRate /= 0.9;
+	console.log(video.playbackRate)
 });
 //faster
-document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause")
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log(video.currentTime);
+	video.currentTime += 5
+	console.log(video.currentTime)
 });
 //skip
-document.querySelector("#pause").addEventListener("change", function() {
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log(video.muted)
+	if (video.muted == false){
+		video.muted = true
+	}
+	else{
+		this.innerHTML = "Unmute"
+		video.muted = false
+	}
+	console.log(video.muted)
+});
+//mute
+
+document.querySelector("#volumeSlider").addEventListener("change", function() {
 	console.log("Volume Slider");
 	video.volume = this.value /100
 	console.log(this.value)
 });
-//mute
-
-document.querySelector("#volumeSlider").addEventListener("click", function() {
-	console.log("Pause")
-});
-document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause")
+document.querySelector("#original").addEventListener("click", function() {
+	console.log("Original");
+	video.classList.remove("oldTime");
 });
 //old
-document.querySelector("#pause").addEventListener("click", function() {
-	console.log("Pause")
+document.querySelector("#old").addEventListener("click", function() {
+	console.log("Old School");
+	video.classList.add("oldTime");
 });
 
